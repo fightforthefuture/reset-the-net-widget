da0adee9e6720f5214a707d72cecd1387a1b6a09
Jeff L <jeff@rubbingalcoholic.com>
2014-06-01 02:28:40 -0700

    initial checkin of main code, README tweaks lol

---
+++ README.md
@@ -0,0 +1,33 @@
+Reset the Net Widget
+=======================
+#### Let's encrypt all the things!
+
+[**Main project site here!**][1]
+
+[**See the widget in action here!**][2]
+
+[**See the widget in action (mobile version) here!**][3]
+
+
+Introduction
+------------
+This is a widget to allow webmasters to embed an RTN banner on their site.
+I will write a whole book about it when I have more time.
+
+
+Installation instructions
+-------------------------
+Please see the demo pages above and do your best to reverse engineer them.
+It's nearly 3am or I'd put something here.
+
+
+Testing instructions
+--------------------
+Yep.
+
+
+
+
+[1]: https://www.resetthenet.org
+[2]: http://rubbingalcoholic.com/demos/rtn_widget/demo.html
+[2]: http://rubbingalcoholic.com/demos/rtn_widget/demo_mobile.html
---
+++ widget/rtn.js
@@ -0,0 +1,159 @@
+if (typeof _rtn_options == "undefined") _rtn_options = {};
+if (typeof _rtn_options.iframe_base_path == "undefined") _rtn_options.iframe_base_path = 'https://fightforthefuture.github.io/rtn_widget/widget/iframe';
+if (typeof _rtn_options.animation == "undefined") _rtn_options.animation = 'main';
+if (typeof _rtn_options.delay == "undefined") _rtn_options.delay = 0;
+if (typeof _rtn_options.debug == "undefined") _rtn_options.debug = false;
+if (typeof _rtn_options.always_show_widget == "undefined") _rtn_options.always_show_widget = false;
+
+var _rtn_animations = {
+	main: {
+		options: {
+			modalAnimation: 'main'
+		},
+		init: function(options) {
+			for (var k in options) this.options[k] = options[k];
+			return this;
+		},
+		start: function() {
+			var iframe = _rtn_util.createIframe();
+			_rtn_util.bindIframeCommunicator(document.getElementById('_rtn_iframe'), this);
+		},
+		stop: function() {
+			_rtn_util.destroyIframe();
+		}
+	}
+}
+
+var _rtn_util = {
+	injectCSS: function(id, css)
+	{
+		var style = document.createElement('style');
+		style.type = 'text/css';
+		style.id = id;
+		if (style.styleSheet) style.styleSheet.cssText = css;
+		else style.appendChild(document.createTextNode(css));
+		document.head.appendChild(style);
+	},
+	createIframe: function() {
+		var iframe = document.createElement('iframe');
+		iframe.id = '_rtn_iframe';
+		iframe.src = _rtn_options.iframe_base_path + '/iframe.html';
+		iframe.frameBorder = 0;
+		iframe.allowTransparency = true; 
+		iframe.style.display = 'none';
+		document.body.appendChild(iframe);
+		return iframe;
+	},
+	destroyIframe: function() {
+		var iframe = document.getElementById('_rtn_iframe');
+		iframe.parentNode.removeChild(iframe);
+	},
+	bindIframeCommunicator: function(iframe, animation) {
+		var sendMessage = function(requestType, data)
+		{
+			data || (data = {});
+			data.requestType = requestType;
+			data.RTN_WIDGET_MSG = true;
+			iframe.contentWindow.postMessage(data, '*');
+		}
+
+		var eventMethod = window.addEventListener ? "addEventListener" : "attachEvent";
+		var eventer = window[eventMethod];
+		var messageEvent = eventMethod == "attachEvent" ? "onmessage" : "message";
+
+		eventer(messageEvent,function(e) {
+			if (!e.data || !e.data.RTN_IFRAME_MSG)
+				return;
+
+			delete e.data.RTN_IFRAME_MSG;
+
+			switch (e.data.requestType) {
+				case 'getAnimation':
+					iframe.style.display = 'block';
+					sendMessage('putAnimation', animation.options);
+					break;
+				case 'stop':
+					animation.stop();
+					break;
+			}
+		}, false);
+
+	},
+	setCookie: function(name,val,exdays)
+	{
+		var d = new Date();
+		d.setTime(d.getTime()+(exdays*24*60*60*1000));
+		var expires = "expires="+d.toGMTString();
+		document.cookie = name + "=" + val + "; " + expires;
+	},
+	getCookie: function(cname)
+	{
+		var name = cname + "=";
+		var ca = document.cookie.split(';');
+		for(var i=0; i<ca.length; i++)
+  		{
+  			var c = ca[i].trim();
+  			if (c.indexOf(name)==0)
+  				return c.substring(name.length,c.length);
+  		}
+		return "";
+	},
+	log: function() {
+		if (_rtn_options.debug)
+			console.log.apply(console, arguments);
+	}
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+	
+	// Don't show the widget more than once, unless we really want it.
+	if 	(
+		_rtn_util.getCookie('_RTN_WIDGET_SHOWN')
+		&&
+		window.location.href.indexOf('ALWAYS_SHOW_RTN_WIDGET') == -1
+		&&
+		_rtn_options.always_show_widget == false
+		)
+		return;
+	else
+		_rtn_util.setCookie('_RTN_WIDGET_SHOWN', 'true', 365);
+
+	_rtn_util.injectCSS('_rtn_iframe_css', '#_rtn_iframe { position: fixed; left: 0px; top: 0px; width: 100%; height: 100%; }');
+
+	var images = new Array()
+	var preloaded = 0;
+
+	function preload() {
+		for (i = 0; i < preload.arguments.length; i++) {
+			images[i] = new Image()
+			images[i].src = _rtn_options.iframe_base_path + '/' + preload.arguments[i]
+			images[i].onload = function() {
+				preloaded++;
+				_rtn_util.log('Preloaded ' + preloaded + ' images.');
+				if (preloaded == images.length)
+				{
+					_rtn_util.log('DONE PRELOADING IMAGES. Starting animation in ' + _rtn_options.delay + ' milliseconds.');
+					setTimeout(function() {
+						var animation = _rtn_animations[_rtn_options.animation].init(_rtn_options).start();
+					}, _rtn_options.delay);
+				}
+			}
+		}
+	}
+
+	// Preload images before showing the animation
+	preload(
+		'background.png',
+		'button.png',
+		'button_glow.png',
+		'camera.png',
+		'houses_back.png',
+		'houses_front.png',
+		'logo.png',
+		'record_light.png'
+	);
+
+}, false);
+
+
+
